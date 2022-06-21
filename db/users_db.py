847803a03a78bc56529ca3c1907bf3edbd773194
polarbear4 <99217094+polarbear4@users.noreply.github.com>
2022-06-21 22:08:01 +0500

    Update users_db.py

--- db/users_db.py
+++ db/users_db.py
@@ -9,6 +9,10 @@ def strart():
     cursor.execute("CREATE TABLE IF NOT EXISTS users(id INT, user_name TEXT)")
     
 
-async def add_new_user(id, user_name):
-    cursor.execute("INSERT INTO users VALUES(?,?)", (id, user_name,))
-    base.commit()
+async def add_new_user(user_id, user_name):
+    check = cursor.execute(f"SELECT id FROM users WHERE id={user_id}").fetchone()
+    if check is None:
+        cursor.execute("INSERT INTO users VALUES(?,?)", (user_id, user_name,))
+        base.commit()
+    else:
+        return
