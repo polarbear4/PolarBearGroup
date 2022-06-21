bbaa9b8bd79656a98e866f01451b6a324d47b667
polarbear4 <99217094+polarbear4@users.noreply.github.com>
2022-06-21 21:34:21 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from aiogram.utils import executor
+from users import gretting
+from db import users_db
+import loader
+
+
+def on_startup(_):
+    users_db.strart()
+
+
+
+gretting.register_message_handler_user(loader.dp)
+
+
+if __name__ == "__main__":
+    executor.start_polling(loader.dp, skip_updates=True, on_startup=on_startup)
