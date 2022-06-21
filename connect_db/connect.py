3bab45017472ff77301bea21478ccd6da59920fa
polarbear4 <99217094+polarbear4@users.noreply.github.com>
2022-06-21 22:01:04 +0500

    Update connect.py

--- connect_db/connect.py
+++ connect_db/connect.py
@@ -2,5 +2,5 @@ from db import users_db
 
 
 
-async def connect_to_user_db(id, user_name):
-    await users_db.add_new_user(id, user_name)
+async def connect_to_user_db(user_id, user_name):
+    await users_db.add_new_user(user_id, user_name)
