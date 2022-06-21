7846f97df14edd21ff2d3abb3ced7fc29982071c
polarbear4 <99217094+polarbear4@users.noreply.github.com>
2022-06-21 21:56:52 +0500

    Update gretting.py

--- users/gretting.py
+++ users/gretting.py
@@ -7,10 +7,10 @@ async def gretting(message: types.Message):
     await message.answer(f"Hello my dear {message.from_user.full_name}\n" 
                         "I want to give you inforamtion about our telegram groups <strong><pre>'infomation'," 
                         "'your rules or others'</pre></strong>", parse_mode="HTML")
-    id = message.from_user.id
+    user_id = message.from_user.id
     user_name = message.from_user.full_name
 
-    await connect.connect_to_user_db(id, user_name)
+    await connect.connect_to_user_db(user_id, user_name)
 
 
 
